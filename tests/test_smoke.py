694d12068ed6da9aea99817142b2ad9b029e4afc
comet <yungag@naver.com>
2026-04-14 22:07:10 +0900

    test: improve smoke tests with meaningful validation

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,2 +1,14 @@
-def test_ci_smoke():
-    assert True
+def test_imports():
+    import pages.login_page
+    import config
+    import data.login_data
+
+
+def test_login_page_init():
+    from pages.login_page import LoginPage
+
+    class DummyDriver:
+        pass
+
+    page = LoginPage(DummyDriver())
+    assert page is not None
